bb5b65590fea6c3a72f5ff4075e45684971831a0
Mauricio-Alonso <conta.alonso@gmail.com>
2022-09-24 16:44:07 -0300

    Se creó la estructura inicial del proyecto y  se cargó bootstrap.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
+
+const App = () => {
+  return (
+<Container className='my-5'>
+  <h1 className='diplay4'> LOS SIMPSONS</h1>
+  <hr />
+</Container>
+  );
+};
+
+export default App;
