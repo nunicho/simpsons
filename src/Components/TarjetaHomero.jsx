79d33ed0ae6967ae9bd4b61bd60e7553f720e413
Mauricio-Alonso <conta.alonso@gmail.com>
2022-09-24 18:00:11 -0300

    Ajustes al dev.

--- src/Components/TarjetaHomero.jsx
+++ src/Components/TarjetaHomero.jsx
@@ -3,7 +3,8 @@ import imageHomero from '../img/imageHomero.png'
 
 const TarjetaHomero = () => {
     return (
-        <div className="container row rounded my-5">
+        <div className="container row rounded mx-1
+         my-5 bg-light p-3">
            <img src={imageHomero} className="img-fluid col-4" alt="imageHomero" /> 
            <div className="col-8"> 
            <p className="fw-bold"> Homer Simpson</p>
