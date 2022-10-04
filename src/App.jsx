e3407987306b576d6d372a44fbb154fe3d7ce1c7
Mauricio-Alonso <conta.alonso@gmail.com>
2022-10-03 21:58:20 -0300

    Terminada la mecánica API.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Button, Container} from 'react-bootstrap';
+import {Container} from 'react-bootstrap';
 import Frase from './Components/Frase';
 import Character from  './Components/Character'
 import { useEffect, useState } from 'react';
@@ -33,20 +33,23 @@ setPersonaje(dato[0]);
   return (
    <Container className=" py-5 fondoPrincipal">
   <article>         
-        <div className="container">
-           <img src={imageSimpsons} className="img-fluid" alt="logoSimpsons my-3" /> 
+        <section className="container text-center">
+           <img src={imageSimpsons} className="w-75" alt="logoSimpsons my-3" /> 
            <div className="text-center my-3 ">
-           <Button className="botonFrase" onClick={consultarAPI}> Obtener Frase</Button>
+           <button className='botonFrase' onClick={consultarAPI}> Obtener Frase</button>
            </div>
-        </div>
-        <div className="container row rounded mx-1
-         my-5 bg-light p-3">
+        </section>
+        <section >
+        <div className="container row rounded mx-1 my-5 bg-light p-3 text-md-right text-lg-right text-center">
+           <div className="col-12 col-md-6 col-lg-6">
             <Character personaje={personaje}></Character>
-           <div className="col-8"> 
+          </div>
+           <div className="col-12 col-md-6 col-lg-6"> 
            <p className="fw-bold">{personaje.character}</p>
            <Frase personaje={personaje}></Frase>
            </div>
         </div>
+        </section>
   </article>   
   </Container>
   );
